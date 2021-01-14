9724bb2bf2a05b4f95359b5a7cb714dfc0eaca6a
Waldemar Ortiz-Calo <wortizcalo@gmail.com>
2021-01-14 15:12:23 -0700

    Rewriting Dr. Hebblewhite's code

--- 2.R_Code/1.Scripts/Lab1_WOC.R
+++ 2.R_Code/1.Scripts/Lab1_WOC.R
@@ -25,6 +25,7 @@ library(plot3D)
 library(rasterVis)
 library(colorRamps)
 library(rgeos)
+library(mapview)
 
 #      Functions                                                            ####
 
@@ -59,6 +60,8 @@ disthumanaccess2<-raster("1.Data\\Lab1_data\\DistFromHumanAccess2.tif") #resampl
 
 ###############################################################################
 #   Cougar Data Breakdown                                                   ####
+#      Summary Data                                                         ####
+
 ## Cougar summary statistics
 summary(cougar)
 
@@ -85,6 +88,8 @@ boxplot(AllTrails~factorUSE, ylab="Distance (m)", xlab="Used",main = "Trails", d
 boxplot(CoverDist~factorUSE, ylab="Distance (m)", xlab="Used", main = "Cover", data=cougar)
 boxplot(Roads~factorUSE, ylab="Distance (m)", xlab="Used",main = "Roads", data=cougar)
 boxplot(Slope~factorUSE, ylab="Slope", xlab="Used", main = "Slope", data=cougar)
+
+#      Statistical Exploration                                              ####
 
 ## Cougar statistical tests
 
@@ -158,3 +163,118 @@ summary(logitRoads)
 ggplot(cougar, aes(x=Slope, y=UseNonUse)) + geom_rug() + stat_smooth(method="glm", method.args=list(family="binomial"))
 ggplot(cougar, aes(x=CoverDist, y=UseNonUse)) + geom_rug() + stat_smooth(method="glm", method.args=list(family="binomial"))
 #   Spatial Data                                                            ####
+#        [Shapefile]                                                        ####
+# make a very basic plot of shapefile after resetting graphical parameters
+par(mfrow= c(1,1))
+plot(elc_habitat)
+plot(wolfyht)
+
+# look at the class of the shapefile
+class(elc_habitat)
+
+# look at structure of shapefile
+#str(elc_habitat)
+
+# look at first 20 rows of data for shapefile
+head(elc_habitat@data, n=20)
+
+# look at the projection of the shapefile (note the use of "@" instead of "$")
+elc_habitat@proj4string@projargs
+wolfyht@proj4string@projargs
+
+elc_habitat <- spTransform(elc_habitat, CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+
+# check new projection in geographic coordinate system WGS84
+elc_habitat@proj4string@projargs
+
+# reset projection back to what is was previously
+elc_habitat <-  spTransform(elc_habitat, CRS("+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+
+# look at the projection of the shapefile 
+elc_habitat@proj4string@projargs
+
+# another way to change it back to previous 
+elc_habitat <-  spTransform(elc_habitat, CRS("+proj=utm +zone=11 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"))
+
+#write shapefile to files
+writeOGR(elc_habitat,".","elc_habitat_NEW",driver="ESRI Shapefile", overwrite_layer=TRUE)
+
+## Extents
+extent(elc_habitat)
+extent(wolfyht)
+
+#        [Raster]                                                           ####
+
+## reset graphical parameters
+par(mfrow= c(1,1)) 
+
+# make a very basic plot of raster
+plot(deer_w)
+
+# look at the class of the raster
+class(deer_w)
+
+# look at basic raster summary
+deer_w
+
+# look at raster data
+deer_w@data
+
+# look at structure of raster
+str(deer_w)
+
+# look at the projection of the raster (note the use of "@" instead of "$")
+deer_w@crs@projargs
+
+# look at the spatial extent of the raster
+extent(deer_w)
+
+# look at the resolution of the raster
+res(deer_w)
+
+#And if we need to change the projection of the raster to another projection (package rgdal) - note for the Tutorial I've masked these out # because they take a LONG time. 
+
+#deer_w2 <- projectRaster(deer_w, crs="+init=epsg:4326")
+
+## or try this the same way we did above
+
+#deer_w2 <- projectRaster(deer_w, crs="+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0")
+
+#check projection of the raster
+deer_w2@crs@projargs
+
+#change it back to what it was using another raster layers projection
+#deer_w <- projectRaster(deer_w2, wolf_w)
+
+#check projection of the raster
+deer_w@crs@projargs
+
+## Creating a Raster Stack
+
+#One of the GREAT things about doing GIS in R is the ability to create a consistent raster stack of rasters of the same projection, extent, etc.  LEts create a raster stack!
+  
+all.rasters<-stack(deer_w, moose_w, elk_w, sheep_w, goat_w, elevation2, disthumanaccess2)
+
+plot(all.rasters)
+
+#check class
+class(all.rasters)
+
+#learn more about 
+#?writeRaster()
+writeRaster(deer_w, "1.Data\\Lab1_data\\new_deer.tiff", "GTiff", overwrite=TRUE)
+
+# Mapping with Mapview
+
+mapView(wolfyht2, zcol="NAME", native.crs = FALSE, legend = TRUE, cex=5, lwd=2, map.type = "Esri.DeLorme")
+
+mapView(wolfyht, zcol="NAME", legend = TRUE, cex=5, lwd=2, map.type = "Esri.WorldImagery")
+
+## Open Topo Map
+mapView(wolfyht, zcol="NAME", legend = TRUE, cex=5, lwd=2, map.type = "OpenTopoMap")
+
+## Esri World Topo Map
+mapView(wolfyht, zcol="NAME", legend = TRUE, cex=5, lwd=2, map.type = "Esri.WorldTopoMap")
+
+
+
